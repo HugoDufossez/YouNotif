2c2761ff5560f2a5b479aac73c919e2a9814f8fe
Moumoush <mehdi.bouchagour@laposte.net>
2014-10-09 14:58:21 +0200

    action bar

--- YouNotif/src/com/example/younotif/MainActivity.java
+++ YouNotif/src/com/example/younotif/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.younotif;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -18,7 +19,8 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
 
@@ -27,10 +29,17 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+    	switch (item.getItemId()) {
+        
+        case R.id.action_refresh:
+            // refresh
             return true;
+        case R.id.action_new:
+            // help action
+            return true;
+        
+        default:
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
